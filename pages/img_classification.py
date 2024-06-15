944aa21f87f21dcf763d81d3368c520bfb182dca
wkdlzkk <161596457+wkdlzkk@users.noreply.github.com>
2024-06-16 06:57:53 +0800

    Update img_classification.py

--- pages/img_classification.py
+++ pages/img_classification.py
@@ -17,7 +17,6 @@ drive.mount('/content/drive')
 import os
 import pickle
 import numpy as np
-
 from img2vec_pytorch import Img2Vec
 from PIL import Image, UnidentifiedImageError
 from sklearn.ensemble import RandomForestClassifier
@@ -31,8 +30,6 @@ data_dir = '/content/drive/MyDrive/dataset'
 train_dir = os.path.join(data_dir, 'train')
 val_dir = os.path.join(data_dir, 'val')
 
-
-
 data = {}
 for j, dir_ in enumerate([train_dir, val_dir]):
     features = []
@@ -40,13 +37,13 @@ for j, dir_ in enumerate([train_dir, val_dir]):
     for category in os.listdir(dir_):
         for img_path in os.listdir(os.path.join(dir_, category)):
             img_path_ = os.path.join(dir_, category, img_path)
-
-            img = Image.open(img_path_).convert('RGB')
-
-            img_features = img2vec.get_vec(img)
-
-            features.append(img_features)
-            labels.append(category)
+            try:
+                img = Image.open(img_path_).convert('RGB')
+                img_features = img2vec.get_vec(img)
+                features.append(img_features)
+                labels.append(category)
+            except UnidentifiedImageError:
+                print(f"Error in loading image {img_path_}")
 
     data[['training_data', 'validation_data'][j]] = features
     data[['training_labels', 'validation_labels'][j]] = labels
@@ -80,16 +77,12 @@ print(data.keys())
 param_grid = {
     'C': [0.1, 1, 10, 100],
     'gamma': [1, 0.1, 0.01, 0.001],
-     'kernel': ['rbf', 'poly'],
-
+    'kernel': ['rbf', 'poly'],
 }
 
-
-model = GridSearchCV(SVC(probability=True), param_grid, refit=True, verbose=2,scoring='accuracy' )
+model = GridSearchCV(SVC(probability=True), param_grid, refit=True, verbose=2, scoring='accuracy')
 
 model.fit(data['training_data'], data['training_labels'])
-
-
 
 print(model.best_params_)
 print(model.best_score_)
@@ -97,14 +90,10 @@ print(model.best_score_)
 # save the model
 with open('/content/foods.p', 'wb') as f:
     pickle.dump(model, f)
-    f.close()
 
 import pickle
-
 from img2vec_pytorch import Img2Vec
 from PIL import Image
-
-
 
 with open('/content/foods.p', 'rb') as f:
     model = pickle.load(f)
@@ -114,7 +103,6 @@ class_labels = ['-K', '-N', '-P', 'FN']
 image_path = '/content/drive/MyDrive/dataset/train/pizza/pizza3.jpg'
 
 img = Image.open(image_path)
-
 features = img2vec.get_vec(img)
 features_2d = features.reshape(1, -1)
 
@@ -124,66 +112,66 @@ for ind, prob in enumerate(prediction_probabilities):
     print(f'Class {class_labels[ind]}: {prob*100:.2f}%')
 
 pred = model.predict([features])
-
 print(pred)
 
-# Commented out IPython magic to ensure Python compatibility.
-# %%writefile app.py
-# import pickle
-# from PIL import Image
-# from io import BytesIO
-# from img2vec_pytorch import Img2Vec
-# import streamlit as st
-# 
-# with open('foods.p', 'rb') as f:
-#     model = pickle.load(f)
-# 
-# img2vec = Img2Vec()
-# 
-# ## Streamlit Web App Interface
-# st.set_page_config(layout="wide", page_title="Image Classification for Foods")
-# 
-# st.write("## Image Classification Model in Python!")
-# st.write(
-#     ":grin: We'll try to predict the image on what features it was trained via the uploaded image :grin:"
-# )
-# st.sidebar.write("## Upload and download :gear:")
-# 
-# MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
-# 
-# # Download the fixed image
-# @st.cache_data
-# def convert_image(img):
-#     buf = BytesIO()
-#     img.save(buf, format="jpg")
-#     byte_im = buf.getvalue()
-#     return byte_im
-# 
-# def fix_image(upload):
-#     image = Image.open(upload)
-#     col1.write("Image to be predicted : camera :")
-#     col1.image(image)
-# 
-#     col2.write("Category : foods :")
-#     img = Image.open(my_upload)
-#     features = img2vec.get_vec(img)
-#     pred = model.predict([features])
-# 
-#     col2.header(pred)
-# 
-# 
-# 
-# col1, col2 = st.columns(2)
-# my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-# 
-# if my_upload is not None:
-#     if my_upload.size > MAX_FILE_SIZE:
-#         st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
-#     else:
-#         fix_image(upload=my_upload)
-# else:
-#     st.write("by Vernie Jaica Danico...")
+# Streamlit app for deployment
+with open('app.py', 'w') as f:
+    f.write("""
+import pickle
+from PIL import Image
+from io import BytesIO
+from img2vec_pytorch import Img2Vec
+import streamlit as st
 
-! wget -q -O - ipv4.icanhazip.com
+with open('foods.p', 'rb') as f:
+    model = pickle.load(f)
 
-! streamlit run app.py & npx localtunnel --port 8501
+img2vec = Img2Vec()
+
+## Streamlit Web App Interface
+st.set_page_config(layout="wide", page_title="Image Classification for Foods")
+
+st.write("## Image Classification Model in Python!")
+st.write(
+    ":grin: We'll try to predict the image on what features it was trained via the uploaded image :grin:"
+)
+st.sidebar.write("## Upload and download :gear:")
+
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+# Download the fixed image
+@st.cache_data
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="jpg")
+    byte_im = buf.getvalue()
+    return byte_im
+
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Image to be predicted :camera:")
+    col1.image(image)
+
+    col2.write("Category :foods:")
+    img = Image.open(upload)
+    features = img2vec.get_vec(img)
+    pred = model.predict([features])
+
+    col2.header(pred)
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        fix_image(upload=my_upload)
+else:
+    st.write("by Vernie Jaica Danico...")
+    """)
+
+!wget -q -O - ipv4.icanhazip.com
+!streamlit run app.py & npx localtunnel --port 8501
+    """)
+
