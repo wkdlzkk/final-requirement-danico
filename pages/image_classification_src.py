fa171824066368a396a3463ba0d470eb0d40c7b7
wkdlzkk <161596457+wkdlzkk@users.noreply.github.com>
2024-06-16 07:37:45 +0800

    Update image_classification_src.py

--- pages/image_classification_src.py
+++ pages/image_classification_src.py
@@ -5,69 +5,45 @@ st.header('Image Classification App')
 st.subheader('This python code is implemented for Streamlit')
 st.code('''
         import pickle
-from img2vec_pytorch import Img2Vec
-from PIL import Image
-import streamlit as st
-# from rembg import remove
 from PIL import Image
 from io import BytesIO
-import base64
+from img2vec_pytorch import Img2Vec
+import streamlit as st
 
+st.set_page_config(layout="wide", page_title="Image Classification for Foods")
 
-with open('pages/sky_condition_model.p', 'rb') as f:
-    model = pickle.load(f)
-
-img2vec = Img2Vec()
-
-
-
-## Streamlit Web App Interface
-st.set_page_config(layout="wide", page_title="Image Classification for Weather")
-
-st.write("## Let's try to see what weather is in the image!")
-st.write(
-    ":grin: We'll try to predict the weather depicted in your uploaded image :grin:"
-)
+st.write("## Image Classification Model for Foods")
+st.write(":grin: Predicting food categories from uploaded images :grin:")
 st.sidebar.write("## Upload and download :gear:")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
-# Download the fixed image
-@st.cache_data 
-def convert_image(img):
-    buf = BytesIO()
-    img.save(buf, format="jpg")
-    byte_im = buf.getvalue()
-    return byte_im
+# Load the pre-trained model
+with open('/content/foods.p', 'rb') as f:
+    model = pickle.load(f)
 
-@st.cache_data 
-def fix_image(upload):
+img2vec = Img2Vec()
+
+@st.cache
+def classify_image(upload):
     image = Image.open(upload)
-    col1.write("Image to be predicted :camera:")
-    col1.image(image)
+    st.write("### Image to be predicted:")
+    st.image(image)
 
-    # fixed = remove(image)
-    col2.write("Category :wrench:")
-    # image_path = './example/rain.jpeg'
-    img = Image.open(my_upload)
-    features = img2vec.get_vec(img)
+    st.write("### Predicted Category:")
+    features = img2vec.get_vec(image)
     pred = model.predict([features])
 
-    # print(pred)
-    col2.header(pred)
-    # st.sidebar.markdown("\n")
-    # st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
-
+    st.header(pred[0])  # Display the predicted category
 
 col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+my_upload = col1.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
         st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
     else:
-        fix_image(upload=my_upload)
+        classify_image(my_upload)
 else:
-    st.write("by koalatech...")
-    # fix_image("./zebra.jpg")
+    st.write("Upload an image to classify it.")
     ''')
