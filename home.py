229b1d752456ac477f4e2e7cb03b5a03e3bb53b4
wkdlzkk <161596457+wkdlzkk@users.noreply.github.com>
2024-06-16 07:54:57 +0800

    Update home.py

--- home.py
+++ home.py
@@ -68,6 +68,9 @@ Enter your name.
 Describe how you are feeling today.
 Click the "Analyze Sentiment" button to receive feedback.
 Feel free to express yourself and see how the Sentiment Analyzer responds!
+
+st.image("./food_prediction.png")
+
             
 ### ⭐ Star the project on Github  <iframe src="https://ghbtns.com/github-btn.html?user=koalatech&repo=streamlit_web_app&type=star&count=true"  width="150" height="20" title="GitHub"></iframe>   
 """, unsafe_allow_html=True)
