b3aed0142ccd50171b2afe8fe2bcd22c77ef3703
wkdlzkk <161596457+wkdlzkk@users.noreply.github.com>
2024-06-16 06:41:36 +0800

    Update basic_sentiment_analyzer.py

--- pages/basic_sentiment_analyzer.py
+++ pages/basic_sentiment_analyzer.py
@@ -1,5 +1,3 @@
-# do a "pip install streamlit" first 
-
 import streamlit as st
 import pandas as pd
 import pickle
@@ -8,27 +6,21 @@ from nltk.corpus import names
 st.title("A Simple Sentiment Analyzer")
 message = st.text_input("Tell me what you feel today: ")
 
-# Load the trained Naive Bayes classifier from the saved file
-filename = 'pages/sentimentAnalyzerTest_model.sav'
-loaded_model = pickle.load(open(filename, 'rb'))
+# Define lists of positive and negative words
+positive_words = ['good', 'excited', 'happy', 'great', 'fantastic', 'wonderful']
+negative_words = ['bad', 'sad', 'angry', 'terrible', 'awful', 'miserable']
 
-# Define features (words) and their corresponding labels (positive/negative)
-# @st.cache_data 
-def word_features(words):
-    return dict([(word, True) for word in words])
-
-message_tone = loaded_model.classify(word_features(message.split()))
-
-# make a function for your button click
-# @st.cache_data 
+# Function to classify the sentiment and display a message
 def sayFeeling():
-    # Classify the sentiment
-    if message_tone == 'positive':
-        st.write("this is :smile:")
+    words = message.lower().split()
+    if any(word in positive_words for word in words):
+        st.write("That's good! :smile:")
+    elif any(word in negative_words for word in words):
+        st.write("I hope you feel better soon. :disappointed:")
     else:
-        st.write("this is :disappointed:")
-        
+        st.write("This is neutral. Keep going! :neutral_face:")
+
 st.button('Say it', on_click=sayFeeling)
 
-#to run on terminal issue this command
+# To run on terminal issue this command
 # python -m streamlit run streamlit_test.py
