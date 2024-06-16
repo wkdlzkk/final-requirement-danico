f3aec03e68550c95ac06293b286e1f1e0d6ca920
wkdlzkk <161596457+wkdlzkk@users.noreply.github.com>
2024-06-17 01:11:28 +0800

    Update home.py

--- home.py
+++ home.py
@@ -46,58 +46,18 @@ into a rewarding career.
 ### 🔎 Overview""", unsafe_allow_html=True)
 
 
-st.image("./sentiment_analyzer.png")
 
 
 st.markdown("""
-This application is designed to analyze the sentiment expressed in text messages. It utilizes a trained Naive Bayes classifier to classify text into positive, negative, or neutral sentiments based on the words used.
 
-How it Works:
-
-Input: Users input their name and describe how they are feeling today in the provided text boxes.
-Analysis: The application examines the words in the input message to determine its sentiment.
-Classification: Words are compared against predefined lists of positive, negative, and neutral words. If a match is found, the sentiment is displayed accordingly.
-Feedback: Additionally, users receive motivational messages tailored to their sentiment to uplift their mood.
-Output: The application outputs the user's name along with an assessment of their sentiment and an encouraging message.
-Key Features:
-
-Customizable: Users can input their name and express their feelings freely.
-Real-time Analysis: Sentiment analysis is performed instantly upon clicking the "Analyze Sentiment" button.
-Feedback: Users receive personalized messages to help them feel better based on their sentiment.
-Try it Out:
-
-Enter your name.
-Describe how you are feeling today.
-Click the "Analyze Sentiment" button to receive feedback.
-Feel free to express yourself and see how the Sentiment Analyzer responds!
-   
+--------------------------   
 """, unsafe_allow_html=True)
 
-st.image("./food_prediction.png")
+
 st.markdown("""
-This web application leverages a machine learning model to classify uploaded images based on food categories. Using a pre-trained model, it analyzes features extracted from images to predict and display the category of food depicted.
 
-How it Works:
+-----------------------------
 
-Input: Users upload an image containing food items using the sidebar file uploader.
-
-Analysis: The application extracts visual features from the uploaded image using a deep learning model.
-
-Classification: The extracted features are processed through a trained classifier, which predicts the category of food in the image.
-
-Output: The predicted food category is displayed alongside the uploaded image, providing instant classification results.
-
-Key Features:
-
-Efficient Classification: Utilizes image embeddings for accurate food category prediction.
-User-friendly Interface: Simple file upload interface with real-time classification feedback.
-Scalable: Handles image uploads up to 5MB in size for comprehensive food image analysis.
-Try it Out:
-
-Upload: Select an image file containing food items.
-Predict: Click the "Predict Category" button to see the predicted food category.
-Explore: Explore various food images and see how accurately they are classified!
-Explore the world of food through the lens of machine learning with our Image Classification for Food web application!
 """, unsafe_allow_html=True)
 
 
