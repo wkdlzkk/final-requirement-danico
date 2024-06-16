4770d9ed812a5620905446a825ffb14464ac1770
wkdlzkk <161596457+wkdlzkk@users.noreply.github.com>
2024-06-16 09:06:33 +0800

    Update accuracy.py

--- pages/accuracy.py
+++ pages/accuracy.py
@@ -15,7 +15,7 @@ import os
 print(f"Current directory: {os.getcwd()}")
 
 # Load the trained Random Forest classifier from the saved file
-model_path = 'prediction.p'
+model_path = 'pages/prediction.p'
 
 # Check if file exists
 if os.path.exists(model_path):
