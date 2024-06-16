c58323205231dcb62ae8923e8ae5c2e1065ac2ed
wkdlzkk <161596457+wkdlzkk@users.noreply.github.com>
2024-06-16 13:30:19 +0800

    Update crop_recom_streamlitapp.py

--- pages/crop_recom_streamlitapp.py
+++ pages/crop_recom_streamlitapp.py
@@ -1,33 +1,32 @@
-#Notes
-# do a "pip install streamlit" first 
-#to run on terminal issue this command
-# python -m streamlit run streamlit_test.py
-
 import streamlit as st
 import pandas as pd
 import pickle
-from nltk.corpus import names
 
 # Load the trained Naive Bayes classifier from the saved file
 filename = 'pages/crop_recom_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
-# # Use the model to make predictions
-@st.cache_data 
-def predict_crop():
-    st.text("The crop is " + crop_name)
-    return
-           
-st.title("Crop Predictor")
-st.subheader("Enter NPK:")
-n_input = st.slider("Nitrogen: ",0,500)
-p_input = st.slider("Phosphorus: ",0,500)
-k_input = st.slider("Potassium: ",0,500)
-if n_input == 0 & p_input == 0 & k_input == 0:
-    crop_name = ""
-else:
-    crop_name = loaded_model.predict([[pd.to_numeric(n_input),pd.to_numeric(p_input),pd.to_numeric(k_input)]])
+# Function to predict crop based on input NPK levels
+def predict_crop(n_input, p_input, k_input):
+    if n_input == 0 and p_input == 0 and k_input == 0:
+        return ""  # Return empty string if all inputs are zero
+    else:
+        # Predict using the loaded model
+        crop_name = loaded_model.predict([[n_input, p_input, k_input]])
+        return crop_name[0]  # Return the predicted crop name
 
-st.text("Predicted crop:")
-st.text_area(label ="",value=crop_name, height =100)
-# st.button('Predict', on_click=predict_crop
+# Streamlit app
+st.title("Crop Predictor")
+st.sidebar.subheader("Enter NPK levels:")
+
+# Input sliders for NPK levels in the sidebar
+n_input = st.sidebar.slider("Nitrogen", 0, 500)
+p_input = st.sidebar.slider("Phosphorus", 0, 500)
+k_input = st.sidebar.slider("Potassium", 0, 500)
+
+# Predicting the crop based on NPK levels
+crop_name = predict_crop(n_input, p_input, k_input)
+
+# Display the predicted crop name in the main area
+st.subheader("Predicted crop:")
+st.write(crop_name)
