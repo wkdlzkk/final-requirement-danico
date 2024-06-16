eb6506dc90f7be3e444ae1a7bff10262a6fffa55
wkdlzkk <161596457+wkdlzkk@users.noreply.github.com>
2024-06-16 13:29:32 +0800

    Update crop_recom_streamlitapp.py

--- pages/crop_recom_streamlitapp.py
+++ pages/crop_recom_streamlitapp.py
@@ -18,8 +18,8 @@ def predict_crop():
     st.text("The crop is " + crop_name)
     return
            
-st.title("Crop Recommendation Predictor :smile:")
-st.subheader("Enter a set of NPK levels to determine what crop best fits:")
+st.title("Crop Predictor")
+st.subheader("Enter NPK:")
 n_input = st.slider("Nitrogen: ",0,500)
 p_input = st.slider("Phosphorus: ",0,500)
 k_input = st.slider("Potassium: ",0,500)
@@ -28,6 +28,6 @@ if n_input == 0 & p_input == 0 & k_input == 0:
 else:
     crop_name = loaded_model.predict([[pd.to_numeric(n_input),pd.to_numeric(p_input),pd.to_numeric(k_input)]])
 
-st.text("The crop suitable for this NPK level:")
+st.text("Predicted crop:")
 st.text_area(label ="",value=crop_name, height =100)
 # st.button('Predict', on_click=predict_crop
